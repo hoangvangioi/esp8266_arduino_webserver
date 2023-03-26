0aac4ea9a8315ccf1ca0106ec15d69feee034f14
Hoàng Văn Giỏi <gioitube2k2@gmail.com>
2023-03-26 16:51:12 +0700

    add headers to Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -15,7 +15,7 @@ class DHT11ListData(APIView):
     def get(self, request):
         queryset = DHT11.objects.all()
         serializer = DHT11DataSerializer(queryset, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, headers={'Access-Control-Allow-Origin': '*'})
     def post(self, request):
         # Convert received time from seconds to datetime format
         newDataDict = request.data
@@ -26,9 +26,8 @@ class DHT11ListData(APIView):
         serializer = DHT11DataSerializer(data=newDataDict)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED) 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers={'Access-Control-Allow-Origin': '*'}) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST, headers={'Access-Control-Allow-Origin': '*'})
 
 class DHT11LastRecordData(APIView):
 
@@ -39,7 +38,7 @@ class DHT11LastRecordData(APIView):
             last_record = DHT11()
             last_record.record_id=0
         serializer = DHT11DataSerializer(last_record)
-        return Response(serializer.data)
+        return Response(serializer.data, headers={'Access-Control-Allow-Origin': '*'})
 
 
 class DHT11DataDetail(APIView):
@@ -53,20 +52,20 @@ class DHT11DataDetail(APIView):
     def get(self, request, pk):
         queryset = self.get_object(pk)
         serializer = DHT11DataSerializer(queryset)
-        return Response(serializer.data)
+        return Response(serializer.data, headers={'Access-Control-Allow-Origin': '*'})
 
     def put(self, request, pk):
         queryset = self.get_object(pk)
         serializer = DHT11DataSerializer(queryset, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, headers={'Access-Control-Allow-Origin': '*'})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST, headers={'Access-Control-Allow-Origin': '*'})
 
     def delete(self, request, pk):
         queryset = self.get_object(pk)
         queryset.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT, headers={'Access-Control-Allow-Origin': '*'})
 
 
 class LightListData(APIView):
@@ -74,7 +73,7 @@ class LightListData(APIView):
     def get(self, request):
         queryset = Light.objects.all()
         serializer = LightDataSerializer(queryset, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, headers={'Access-Control-Allow-Origin': '*'})
     def post(self, request):
         # Convert received time from seconds to datetime format
         newDataDict = request.data
@@ -86,8 +85,8 @@ class LightListData(APIView):
         serializer = LightDataSerializer(data=newDataDict)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED) 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers={'Access-Control-Allow-Origin': '*'}) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST, headers={'Access-Control-Allow-Origin': '*'})
 
 
 class LightDataDetail(APIView):
@@ -101,20 +100,20 @@ class LightDataDetail(APIView):
     def get(self, request, pk):
         queryset = self.get_object(pk)
         serializer = LightDataSerializer(queryset)
-        return Response(serializer.data)
+        return Response(serializer.data, headers={'Access-Control-Allow-Origin': '*'})
 
     def put(self, request, pk):
         queryset = self.get_object(pk)
         serializer = LightDataSerializer(queryset, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, headers={'Access-Control-Allow-Origin': '*'})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST, headers={'Access-Control-Allow-Origin': '*'})
 
     def delete(self, request, pk):
         queryset = self.get_object(pk)
         queryset.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT, headers={'Access-Control-Allow-Origin': '*'})
     
 
 class SoundListData(APIView):
@@ -122,7 +121,7 @@ class SoundListData(APIView):
     def get(self, request):
         queryset = Sound.objects.all()
         serializer = SoundDataSerializer(queryset, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, headers={'Access-Control-Allow-Origin': '*'})
     def post(self, request):
         # Convert received time from seconds to datetime format
         newDataDict = request.data
@@ -134,8 +133,8 @@ class SoundListData(APIView):
         serializer = SoundDataSerializer(data=newDataDict)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED) 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers={'Access-Control-Allow-Origin': '*'}) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST, headers={'Access-Control-Allow-Origin': '*'})
 
 
 class SoundDataDetail(APIView):
@@ -149,20 +148,20 @@ class SoundDataDetail(APIView):
     def get(self, request, pk):
         queryset = self.get_object(pk)
         serializer = SoundDataSerializer(queryset)
-        return Response(serializer.data)
+        return Response(serializer.data, headers={'Access-Control-Allow-Origin': '*'})
 
     def put(self, request, pk):
         queryset = self.get_object(pk)
         serializer = SoundDataSerializer(queryset, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, headers={'Access-Control-Allow-Origin': '*'})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST, headers={'Access-Control-Allow-Origin': '*'})
 
     def delete(self, request, pk):
         queryset = self.get_object(pk)
         queryset.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT, headers={'Access-Control-Allow-Origin': '*'})
 
 
 class WaterSensorListData(APIView):
@@ -170,7 +169,7 @@ class WaterSensorListData(APIView):
     def get(self, request):
         queryset = WaterSensor.objects.all()
         serializer = WaterSensorDataSerializer(queryset, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, headers={'Access-Control-Allow-Origin': '*'})
     def post(self, request):
         # Convert received time from seconds to datetime format
         newDataDict = request.data
@@ -182,8 +181,8 @@ class WaterSensorListData(APIView):
         serializer = WaterSensorDataSerializer(data=newDataDict)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED) 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers={'Access-Control-Allow-Origin': '*'}) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST, headers={'Access-Control-Allow-Origin': '*'})
 
 
 class WaterSensorDataDetail(APIView):
@@ -197,17 +196,17 @@ class WaterSensorDataDetail(APIView):
     def get(self, request, pk):
         queryset = self.get_object(pk)
         serializer = WaterSensorDataSerializer(queryset)
-        return Response(serializer.data)
+        return Response(serializer.data, headers={'Access-Control-Allow-Origin': '*'})
 
     def put(self, request, pk):
         queryset = self.get_object(pk)
         serializer = WaterSensorDataSerializer(queryset, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, headers={'Access-Control-Allow-Origin': '*'})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST, headers={'Access-Control-Allow-Origin': '*'})
 
     def delete(self, request, pk):
         queryset = self.get_object(pk)
         queryset.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT, headers={'Access-Control-Allow-Origin': '*'})
